a2abc9233ca634fbea5cb2550d2d02917a11fbc6
0XAymax <aymanderrouich3@gmail.com>
2025-07-04 01:48:49 +0100

    update forgot pass link

--- src/app/(marketing)/(auth)/sign-in/page.tsx
+++ src/app/(marketing)/(auth)/sign-in/page.tsx
@@ -102,7 +102,7 @@ export default function LoginPage() {
               <div className="flex items-center justify-between">
                 <Label htmlFor="password">Password</Label>
                 <Link
-                  href="/forgot-password"
+                  href="/sign-in/forgot-password"
                   className="text-sm text-blue-600 hover:text-blue-500 hover:underline"
                 >
                   Forgot password?
