6963a57d2842fd150341042fd7ebdf8dda971129
0XAymax <aymanderrouich3@gmail.com>
2025-07-05 16:42:21 +0100

    Ading protected and admin procedure

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,41 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { Context } from "./context";
 
-const trpc = initTRPC.create();
+
+const trpc = initTRPC.context<Context>().create();
 
 export const router = trpc.router;
 export const procedure = trpc.procedure;
+
+export const protectedProcedure = trpc.procedure.use(({ ctx, next }) => {
+  if (!ctx.session?.user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in to access this resource",
+    });
+  }
+
+  return next({
+    ctx: {
+      ...ctx,
+      session: ctx.session,
+    },
+  });
+});
+
+
+export const adminProcedure = protectedProcedure.use(async ({ ctx, next }) => {
+  const user = await ctx.prisma.user.findUnique({
+    where: { id: ctx.session.user.id },
+    select: { isAdmin: true },
+  });
+
+  if (!user?.isAdmin) {
+    throw new TRPCError({
+      code: "FORBIDDEN",
+      message: "Admin access required",
+    });
+  }
+
+  return next({ ctx });
+});
