32b9d329ad43e7783a985e7e3b1aa3250617d2cf
0XAymax <aymanderrouich3@gmail.com>
2025-07-07 10:15:59 -0700

    Adding side bar component

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,69 @@
+import { Compass, Globe,Inbox, LogOut, ShieldCheck, UserCircle } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Logo from "./Logo"
+import NavItem from "./nav-item"
+
+const items = [
+  {
+    title: "All Conferences",
+    url: "/dashboard/all-conferences",
+    icon: Globe,
+  },
+  {
+    title: "My Conferences",
+    url: "/dashboard/my-conferences",
+    icon: Compass,
+  },
+  {
+    title: "My Roles",
+    url: "/dashboard/my-roles",
+    icon: ShieldCheck,
+  },
+  {
+    title: "Inbox",
+    url: "/dashboard/inbox",
+    icon: Inbox,
+  },
+]
+const secondaryItems = [
+  {
+    title: "My Account",
+    url: "/dashboard/account",
+    icon: UserCircle,
+  },
+  {
+    title: "Logout",
+    url: "/dashboard/sign-out",
+    icon: LogOut,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+        <SidebarHeader>
+        <div className="flex items-center gap-2 px-2 py-2">
+          <Logo width={100} height={100} />
+        </div>
+        </SidebarHeader>
+      <SidebarContent>
+        <NavItem items={items} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavItem items={secondaryItems} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
