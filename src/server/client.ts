5eacf3ac3a4a2b870929048db177da10284bde2d
0XAymax <aymanderrouich3@gmail.com>
2025-07-28 14:34:22 -0700

    UPDATING URL

--- src/server/client.ts
+++ src/server/client.ts
@@ -3,12 +3,12 @@ import { createTRPCReact } from "@trpc/react-query";
 import { httpBatchLink } from "@trpc/react-query";
 
 export const trpc = createTRPCReact<AppRouter>();
-
+const FRONTEND_URL = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
 export const createTRPCClient = () => {
     return trpc.createClient({
         links: [
             httpBatchLink({
-                url: "http://localhost:3000/api/trpc",
+                url: `${FRONTEND_URL}/api/trpc`,
             }),
         ],
     });
